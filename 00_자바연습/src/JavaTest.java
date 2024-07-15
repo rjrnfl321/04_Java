38b43c89cb1b6f47f54df5b9241f7be5d39eee6c
rjrnfl321 <rjrnfl321@gmail.com>
2024-07-15 17:10:31 +0900

    20240715_Java 수업내용

---
+++ 00_자바연습/src/JavaTest.java
@@ -0,0 +1,28 @@
+// 주석은 JS와 동일
+// 한 줄 주석
+/* 범위 주석 */ 
+
+// Ctrl , +/- : 글씨 크기 조절
+public class JavaTest {
+
+	public static void main(String[] args) {
+		System.out.println("이클립스 사용 중");
+		
+		
+       // Sysout -> ctrl + space -> enter
+		System.out.println("메모장 보다 쉽다");
+		
+		// 실행 : 위에 Run 버튼 () 또는 단축키 ctrl + f11
+		/* Java API DOC 확인 방법
+		 * 	- Java API : 자바가 만들어서 제공해준 코드(기능)
+		 * 		
+		 * 	- API DOC  : 제공하는 기능에 대한 설명 문서
+		 *  궁금한 것을 클릭 -> shift + f2
+		 *  
+		 *  자바 - 컴파일러 : 코드를 2진수로 번역하는 프로그램
+		 */
+	}
+
+}
+
+
