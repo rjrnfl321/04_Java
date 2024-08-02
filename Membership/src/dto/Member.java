31e26f1be531b1b6277aec877da2f413bcd5f8d4
rjrnfl321 <rjrnfl321@gmail.com>
2024-08-02 16:57:05 +0900

    20240802_수업내용

--- Membership/src/dto/Member.java
+++ Membership/src/dto/Member.java
@@ -3,16 +3,19 @@ package dto;
 import java.io.Serializable;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+
 
 @Getter // 컴파일 시 getter 구문을 자동 추가
 @Setter // 컴파일 시 setter 구문을 자동 추가
 @NoArgsConstructor // 기본 생성자 
 @AllArgsConstructor // 전체 필드 매개 변수 생성자
 @ToString // toString() 오버라이딩
+@EqualsAndHashCode // equals(), hashCode() 오버라이딩
 public class Member implements Serializable { // + 직렬화
 	
 	// DTO(Data Transfer Object) : 데이터 전달용 객체
@@ -35,4 +38,12 @@ public class Member implements Serializable { // + 직렬화
 	private String phone;
 	private int amount;
 	private int grade;
+	
+	
+	
+	
+	
+	
+	
+	
 }
